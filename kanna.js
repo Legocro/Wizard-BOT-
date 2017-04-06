0c414f0c4f259e0facdd92899c505442c065ad52
WizardLink <alexandre.cssilva@gmail.com>
2017-04-06 20:28:34 -0300

    Add files via upload

--- kanna.js
+++ kanna.js
@@ -4,25 +4,21 @@
    STEAM: steamcommunity.com/id/gentleman_source
 */
 ////////////////////////////////////
-
 //////////////CONSTANTS/////////////
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const fs = require("fs");
 const jsonfile = require('jsonfile');
 const ddiff = require('return-deep-diff');
-
-var commandUsed = false;
 ////////////////////////////////////
 //--------------------------------//
 ///////////CONSOLE LOGS/////////////
 client.on('ready', () => {
-  console.log("I\'m Online\nAnd\nI need punishment.");
+  console.log("I want some insects!");
 });
 ////////////////////////////////////
 //--------------------------------//
 //////////////BLOCKS////////////////
-let points = JSON.parse(fs.readFileSync('./points.json', 'utf8'));
 let names = JSON.parse(fs.readFileSync('./names.json', 'utf8'));
 ////////////////////////////////////
 //--------------------------------//
@@ -31,17 +27,18 @@ client.on('channelPinsUpdate', (channel, time) => {
   channel.guild.defaultChannel.sendMessage(`**The pins of the channel __${channel}__ have been updated!**`);
 });
 client.on('guildCreate', guild => {
-  guild.defaultChannel.sendMessage(`Thanks for adding me here! I hope ${guild.name} haves loads of insects!`);
-  if (!names[guild.id]) names[guild.id] = {
+  guild.defaultChannel.sendMessage(`**Thanks for adding me here! I hope __${guild.name}__ have loads of insects!\n\nDo \`k!about\` to know a bit about me!\n\nYou can do mod commands by doing a role called "Magician!" and assigning to yourself.\n\nAlso you can see all commands with \`k!help\`!\n\nJoin my support server if you have any questions!\nhttps://discord.gg/uBdXdE9\n\nOh and add my emoji to your server! \`https://cdn.discordapp.com/emojis/282721376320618497.png\` so i can show myself!**`);
+  names[guild.id] = {
     firstName: "Dio",
     lastName: "Brando",
-    quizPhoto: "http://vignette3.wikia.nocookie.net/jjba/images/8/87/DioP2.png/revision/latest?cb=20161028235952"
+    quizPhoto: "http://vignette3.wikia.nocookie.net/jjba/images/8/87/DioP2.png/revision/latest?cb=20161028235952",
   }
+  console.log(`I have joined ${guild.name}, it\'s ID is ${guild.id}!`)
 });
 ////////////////////////////////////
 //--------------------------------//
 //////////CLIENT EVENTS/////////////
-var prefix = "w!";
+var prefix = "k!";
 client.on('message', message => {
   if (message.author.bot) return;
 ////////////EVENT HORIZON///////////
@@ -49,7 +46,6 @@ client.on('message', message => {
 ////////////////////////////////////
   var args = message.content.split(' ').slice(1);
   var argresult = args.join(' ');
-  let userData = points[message.author.id];
   let firstname = names[message.guild.id].firstName;
   let lastname = names[message.guild.id].lastName;
   let qphoto = names[message.guild.id].quizPhoto;
@@ -59,124 +55,87 @@ client.on('message', message => {
     message.channel.sendMessage(`Fear FBI ${message.author}\nhttps://cdn.discordapp.com/attachments/279299921071570958/298142045938122753/memo.png`);
   };
 
+  if (message.content.includes('loli')) {
+    message.channel.sendMessage(`**Hmm... A hooman**\nhttps://cdn.discordapp.com/attachments/275135980481150976/299263322157744128/aKVvBp1_460s.png`)
+  };
+
+  if (message.content.includes('dab' || 'dabs')) {
+    message.channel.sendMessage(`**_dabs_**\nhttps://cdn.discordapp.com/attachments/275135980481150976/299673416712192000/hDrOJlT0GvA.jpg`)
+  };
+
+  if (message.content.includes('drunk')) {
+    message.channel.sendMessage(`**__You have to be this cute to be drunk:__**\nhttps://cdn.discordapp.com/attachments/275135980481150976/299673369706496001/9wl5E10yNqE.jpg`)
+  };
+
   if (message.content.startsWith(prefix + 'ping')) {
     message.channel.sendMessage('Eating insects...').then(msg => {
       msg.edit(`I took \`${msg.createdTimestamp - message.createdTimestamp} ms\` to eat all of them! ${message.guild.emojis.find('name', 'hugme')}`);
       return;
     });
-  } else
+  } else if (message.content.startsWith(prefix + 'about')) {
+    message.channel.sendMessage(`**Who am i?**\n\nWell, i\m **Kanna Kobayashi**! I like insects... I like to fly... Oh and i like Kobayashi-san!\n\n**Join my support server if you have any questions!**\nhttps://discord.gg/uBdXdE9`)
+  } else if (message.content.startsWith(prefix + 'invite')) {
+    message.channel.sendMessage(`Invite me to your server ${message.author}! ${message.guild.emojis.find('name', 'hugme')}\nhttps://discordapp.com/oauth2/authorize?permissions=1341643977&scope=bot&client_id=297459926505095180`)
+  };
 
-  while (userMod) {
-    if (message.content.startsWith(prefix + 'help')) {
-      message.channel.sendMessage(`Konichiwa ${message.author}\n\nThese are the mod commands:\n\`w!restart\`, \`w!sqphoto\`, \`w!sfname\`, \`w!slname\`, \`w!quiz\`, \`w!event\`, \`w!ss\` and \`w!ss\`\n\nAnd these are the pleb ones:\n\`w!ping\`, \`w!guess <character name>\` and \`w!point1s\``);
-    } else if (message.content.startsWith(prefix + 'sg')) {
+  if (message.author.id === '267727230296129536') {
+    if (message.content.startsWith(prefix + 'sg')) {
       if(!argresult) argresult = null;
-      client.user.setGame(argresult);
+      client.user.setGame(argresult + ` | on ${client.guilds.size} servers`);
     } else if (message.content.startsWith(prefix + 'ss')) {
       if(!argresult) argresult = 'online';
       client.user.setStatus(argresult);
+    }
+  };
+
+
+  while (userMod) {
+    if (message.content.startsWith(prefix + 'help')) {
+      message.channel.sendMessage(`Konichiwa ${message.author}\n\nThese are the mod commands:\n\`k!sqphoto\`, \`k!sfname\`, \`k!slname\`, \`k!qstart\` and \`k!event\`\n\nAnd these are the pleb ones:\n\`k!ping\`, \`k!invite\` and \`k!about\``);
     } else if (message.content.startsWith(prefix + 'sfname')) {
       message.channel.sendMessage(`The first name has changed sucessfully ${message.author}`);
-      if (!names[message.guild.id]) names[message.guild.id] = {
-        firstName: args[0]
-      }
       names[message.guild.id].firstName = args[0];
     } else if (message.content.startsWith(prefix + 'slname')) {
       (message.channel.sendMessage(`The last name has changed sucessfully ${message.author}`));
-      if (!names[message.guild.id]) names[message.guild.id] = {
-        lastName: args[0]
-      }
+      names[message.guild.id].lastName = args[0];
     } else if (message.content.startsWith(prefix + 'sqphoto')) {
       (message.channel.sendMessage(`The quiz photo has changed sucessfully ${message.author}`));
-      if (!names[message.guild.id]) names[message.guild.id] = {
-        quizPhoto: args[0]
-      }
-    } else if (message.content.startsWith(prefix + 'quiz')) {
-        (message.guild.defaultChannel.sendMessage("**Try to guess who is this character!**\n" + qphoto));
+      names[message.guild.id].quizPhoto = args[0];
     } else if (message.content.startsWith(prefix + 'event')) {
-      (message.guild.defaultChannel.sendMessage("People around @here! There\'s an quiz event coming!"))
-    } else if (message.content.startsWith(prefix + 'restart')) {
-      (message.channel.sendMessage(`The quiz has restarted sucessfully ${message.author}`))
-      commandUsed = false
-    }
+      (message.guild.defaultChannel.sendMessage("People around @here! There\'s an quiz event coming!"));
+    } else if (message.content.startsWith(prefix + 'qstart')) {
+      message.channel.sendMessage(`The quiz event has been started by ${message.author}!\nIn 15 minutes the event will be ended!\n\n**Try to guess who this is:**\n` + qphoto)
+      .then(() => {
+        message.channel.awaitMessages(response => response.content === firstname || response.content === lastname || response.content === lastname.toLowerCase() || response.content === firstname.toLowerCase() || response.content === firstname + ' ' +  lastname || response.content === firstname.toLowerCase() + ' ' +  lastname.toLowerCase(), {
+          max: 1,
+          time: 900000,
+          errors: ['time'],
+        })
+        .then((collected) => {
+          message.channel.sendMessage(`**${collected.first().author} guessed it right!**\n\n**The character was: __${firstname} ${lastname}__**`);
+        })
+        .catch(() => {
+          message.channel.sendMessage('**No one guessed it right! So the event has ended!**');
+        });
+      });
+    };
       break;
     };
 
   while (!userMod) {
     if (message.content.startsWith(prefix + 'help')) {
-      message.channel.sendMessage(`Hello ${message.author}\n\nThese are the commands you can use:\n\`w!ping\`, \`w!guess <character name>\`, \`w!point1s\``);
+      message.channel.sendMessage(`Hello ${message.author}\n\nThese are the commands you can use:\n\`k!ping\`, \`k!invite\` and \`k!about\``);
     }
     break;
   };
-
-  while (!commandUsed) {
-    if (message.content.startsWith(prefix + 'guess ' + firstname)) {
-      message.channel.sendMessage(`You guessed it right ${message.author}!`);
-      if (!points[message.author.id]) points[message.author.id] = {
-        points : 0
-      }
-      points[message.author.id].points++;
-      commandUsed = true;
-    } else if (message.content.toLowerCase().startsWith(prefix + 'guess' + firstname)) {
-      message.channel.sendMessage(`You guessed it right ${message.author}!`);
-      if (!points[message.author.id]) points[message.author.id] = {
-        points : 0
-      }
-      points[message.author.id].points++;
-      commandUsed = true;
-    } else if (message.content.startsWith(prefix + 'guess' + lastname)) {
-      (message.channel.sendMessage(`You guessed it right ${message.author}!`));
-      if (!points[message.author.id]) points[message.author.id] = {
-        points : 0
-      }
-      points[message.author.id].points++;
-      commandUsed = true;
-    } else if (message.content.toLowerCase().startsWith(prefix + 'guess' + lastname)) {
-      (message.channel.sendMessage(`You guessed it right ${message.author}!`));
-      if (!points[message.author.id]) points[message.author.id] = {
-        points : 0
-      }
-      points[message.author.id].points++;
-      commandUsed = true;
-    }
-    break;
-  };
-
-  switch (commandUsed) {
-    case true:
-    if (message.content.startsWith(prefix + 'guess')) {
-      (message.channel.sendMessage(`**The event has ended ${message.guild.emojis.find('name', 'hugme')}**`));
-    }
-    break;
-    default:
-    if (message.content.startsWith(prefix + 'guess')) {
-      (message.channel.sendMessage(`You guessed it wrong ${message.author}!`));
-    }
-    break;
-  };
-
-  if (points[message.author.id] = 5) {
-    points[message.author.id] = {
-      points: 0
-    }
-  };
-
-  if (message.content.startsWith(prefix + "points")) {
-    message.channel.sendMessage(`**You have currently \`${userData.points}\` point(s) ${message.author}!**`);
-  };
-
-  fs.writeFile('./points.json', JSON.stringify(points), (err) => {
-    if (err) console.error(err)
-  })
 
   fs.writeFile('./names.json', JSON.stringify(names), (err) => {
     if (err) console.error(err)
   });
 ////////////////////////////////////
 });
-//console.log(JSON.parse(fs.readFileSync('./names.json', 'utf8')));
 ////////////////////////////////////
 //--------------------------------//
 ///////////////BOT C////////////////
-client.login("Mjk3NDU5OTI2NTA1MDk1MTgw.C8bytw.AqGX055FzSjy4U1xTKRBf809WxY");
+client.login("Mjk3NDU5OTI2NTA1MDk1MTgw.C8hf_g.hpeJoJTiWoWI64nCEahVH-xCVIk");
 ////////////////////////////////////
