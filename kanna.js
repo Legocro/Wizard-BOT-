3c9595331e5cc499e342b9959970e9684e511991
WizardLink <alexandre.cssilva@gmail.com>
2017-04-09 16:50:46 -0300

    Add files via upload

--- kanna.js
+++ kanna.js
@@ -24,10 +24,10 @@ let names = JSON.parse(fs.readFileSync('./names.json', 'utf8'));
 //--------------------------------//
 //////////GUILD EVENTS//////////////
 client.on('channelPinsUpdate', (channel, time) => {
-  channel.guild.defaultChannel.sendMessage(`**The pins of __${channel}__ have been updated ${message.guild.emojis.find('name', 'kanna')}**`);
+  channel.guild.defaultChannel.sendMessage(`**The pins of __${channel}__ have been updated ${message.guild.emojis.find('name', 'hugme')}**`);
 });
 client.on('guildCreate', guild => {
-  guild.defaultChannel.sendMessage(`**Thanks for adding me here! I hope __${guild.name}__ have loads of insects!\n\nDo \`k!about\` to know a bit about me!\n\nAlso you can see all commands with \`k!help\`!\n\nJoin my support server if you have any questions!\nhttps://discord.gg/uBdXdE9\n\nOh and add my emoji to your server! \`https://cdn.discordapp.com/emojis/282721376320618497.png\` and call it kanna so i can show myself! \(Some commands will appear undefined somewhere because they have that emoji\)**`)
+  guild.defaultChannel.sendMessage(`**Thanks for adding me here! I hope __${guild.name}__ have loads of insects!\n\nDo \`k!about\` to know a bit about me!\n\nAlso you can see all commands with \`k!help\`!\n\nJoin my support server if you have any questions!\nhttps://discord.gg/uBdXdE9\n\nOh and add my emoji to your server! \`https://cdn.discordapp.com/emojis/282721376320618497.png\` and call it hugme so i can show myself! \(Some commands will appear undefined somewhere because they have that emoji\)**`)
   names[guild.id] = {
     firstName: "Kanna",
     lastName: "Kobayashi",
@@ -131,13 +131,13 @@ client.on('message', message => {
 
   if (message.content.startsWith(prefix + 'ping')) {
     message.channel.sendMessage('Eating insects...').then(msg => {
-      msg.edit(`I took \`${msg.createdTimestamp - message.createdTimestamp} ms\` to eat all of them! ${message.guild.emojis.find('name', 'kanna')}`);
+      msg.edit(`I took \`${msg.createdTimestamp - message.createdTimestamp} ms\` to eat all of them! ${message.guild.emojis.find('name', 'hugme')}`);
       return;
     });
   } else if (message.content.startsWith(prefix + 'about')) {
     message.channel.sendMessage(`**こんにちは(Kon'nichiwa) ${message.author}**\n\nI am **カンナカムイ\(Kanna Kamui\)** i live with Kobayashi-san... So call me **Kanna Kobayashi**!\n\nWell, i am at **Discord** to spread the love for anime and quiz!\nFirst of all, you need to assign to yourself a role called \"Dragon Tamer\"\nAfter you can do \`k!qstart\` to test if everything is good!\n\nTo set up a different character you have to do \`k!sfname <first name>\` to set the character first name, \`k!slname <last name>\` to set up the character last name and \`k!sqphoto <link>\` to set up the character photo, then just do \`k!qstart\`!\n\n**Join my support server if you have any questions!**\nhttps://discord.gg/uBdXdE9`)
   } else if (message.content.startsWith(prefix + 'invite')) {
-    message.channel.sendMessage(`Invite me to your server ${message.author}! ${message.guild.emojis.exists('name', 'kanna')}\nhttps://discordapp.com/oauth2/authorize?permissions=1341643977&scope=bot&client_id=297459926505095180`)
+    message.channel.sendMessage(`Invite me to your server ${message.author}! ${message.guild.emojis.exists('name', 'hugme')}\nhttps://discordapp.com/oauth2/authorize?permissions=1341643977&scope=bot&client_id=297459926505095180`)
   };
 
   if (message.author.id === '267727230296129536') {
