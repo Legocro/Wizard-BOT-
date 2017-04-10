45fc00963ec1f87fc42718e171fd3eb6652ef6a9
WizardLink <alexandre.cssilva@gmail.com>
2017-04-09 21:28:09 -0300

    Update kanna.js

--- kanna.js
+++ kanna.js
@@ -137,7 +137,7 @@ client.on('message', message => {
   } else if (message.content.startsWith(prefix + 'about')) {
     message.channel.sendMessage(`**こんにちは(Kon'nichiwa) ${message.author}**\n\nI am **カンナカムイ\(Kanna Kamui\)** i live with Kobayashi-san... So call me **Kanna Kobayashi**!\n\nWell, i am at **Discord** to spread the love for anime and quiz!\nFirst of all, you need to assign to yourself a role called \"Dragon Tamer\"\nAfter you can do \`k!qstart\` to test if everything is good!\n\nTo set up a different character you have to do \`k!sfname <first name>\` to set the character first name, \`k!slname <last name>\` to set up the character last name and \`k!sqphoto <link>\` to set up the character photo, then just do \`k!qstart\`!\n\n**Join my support server if you have any questions!**\nhttps://discord.gg/uBdXdE9`)
   } else if (message.content.startsWith(prefix + 'invite')) {
-    message.channel.sendMessage(`Invite me to your server ${message.author}! ${message.guild.emojis.exists('name', 'hugme')}\nhttps://discordapp.com/oauth2/authorize?permissions=1341643977&scope=bot&client_id=297459926505095180`)
+    message.channel.sendMessage(`Invite me to your server ${message.author}! ${message.guild.emojis.find('name', 'hugme')}\nhttps://discordapp.com/oauth2/authorize?permissions=1341643977&scope=bot&client_id=297459926505095180`)
   };
 
   if (message.author.id === '267727230296129536') {
