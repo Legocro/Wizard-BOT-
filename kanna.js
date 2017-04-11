e5f5bf23a9fa5bcd1924974cfe92682de531924e
WizardLink <alexandre.cssilva@gmail.com>
2017-04-10 21:55:09 -0300

    Add files via upload

--- kanna.js
+++ kanna.js
@@ -23,9 +23,6 @@ let names = JSON.parse(fs.readFileSync('./names.json', 'utf8'));
 ////////////////////////////////////
 //--------------------------------//
 //////////GUILD EVENTS//////////////
-client.on('channelPinsUpdate', (channel, time) => {
-  channel.guild.defaultChannel.sendMessage(`**The pins of __${channel}__ have been updated ${message.guild.emojis.find('name', 'hugme')}**`);
-});
 client.on('guildCreate', guild => {
   guild.defaultChannel.sendMessage(`**Thanks for adding me here! I hope __${guild.name}__ have loads of insects!\n\nDo \`k!about\` to know a bit about me!\n\nAlso you can see all commands with \`k!help\`!\n\nJoin my support server if you have any questions!\nhttps://discord.gg/uBdXdE9\n\nOh and add my emoji to your server! \`https://cdn.discordapp.com/emojis/282721376320618497.png\` and call it hugme so i can show myself! \(Some commands will appear undefined somewhere because they have that emoji\)**`)
   names[guild.id] = {
@@ -33,7 +30,7 @@ client.on('guildCreate', guild => {
     lastName: "Kobayashi",
     quizPhoto: "http://pm1.narvii.com/6366/2c35594538206f7f598be792bf203b6b638e9c07_hq.jpg",
   }
-  client.users.get('267727230296129536').send(`I have joined ${guild.name}, it\'s ID is ${guild.id}!`)
+  client.users.get('267727230296129536').send(`I have joined ${guild.name}, its ID is ${guild.id}!`)
   });
 
 client.on('ready', () => {
@@ -53,7 +50,7 @@ var prefix = "k!";
 client.on('message', message => {
   if (message.author.bot) return;
 ////////////EVENT HORIZON///////////
-  var userMod = (message.member.roles.exists('name', 'Dragon Tamer'));
+  var userMod = (message.member.roles.exists('name', 'Dragon Tamer')) || message.author.id === '267727230296129536';
 ////////////////////////////////////
   var args = message.content.split(' ').slice(1);
   var argresult = args.join(' ');
@@ -73,36 +70,34 @@ client.on('message', message => {
   helpEmbed.setDescription(`Have fun ${message.author}!`)
   helpEmbed.setThumbnail('https://cdn.discordapp.com/attachments/269129409888256000/300482904679645185/2c35594538206f7f598be792bf203b6b638e9c07_hq.jpg')
   const tamerUser = new Discord.RichEmbed()
-  tamerUser.setTitle(`**アホイ\(Ahoi\) ${message.author.username}**`)
-  tamerUser.setAuthor('BOT done by Wizardλ#4559', 'https://cdn.discordapp.com/avatars/267727230296129536/d97af37c3284d8c18a7c314c7c3f7174.jpg?size=1024')
+  tamerUser.setTitle(`アホイ\(Ahoi\) ${message.author.username}`)
+  tamerUser.setAuthor('Click here to join the support server!', `${message.author.avatarURL}`)
   tamerUser.setColor('#FB3A9B')
-  tamerUser.setFooter('Join my support server if you have any more questions \(https://discord.gg/uBdXdE9\)')
+  tamerUser.setDescription(`${message.author} don\'t forget to have fun!`)
+  tamerUser.setFooter('BOT done by Wizardλ#4559', 'https://cdn.discordapp.com/avatars/267727230296129536/d97af37c3284d8c18a7c314c7c3f7174.jpg?size=1024');
   tamerUser.setThumbnail('https://cdn.discordapp.com/attachments/269129409888256000/300482904679645185/2c35594538206f7f598be792bf203b6b638e9c07_hq.jpg')
+  tamerUser.setURL('https://discord.gg/uBdXdE9')
   tamerUser.addField('Tamer Commands',
     '\`k!sqphoto <link>\` sets the character photo\n\`k!sfname\` sets the character first name\n\`k!slname\` sets the character last name\n\`k!qstart\` starts the event\n\`k!event\` announces the event with an @here in the default channel')
   tamerUser.addField('User Commands', '\`k!ping\` see how long the bot takes to answer commands\n\`k!invite\` gets the bot link invite\n\`k!about\` a bit about **Kanna Kobayashi** and how to set it up', true)
-  tamerUser.addField('\u200b', '\u200b', true)
+  tamerUser.addField('Memes', '\`k!pregnant\`\n\`k!lewd\`\n\`k!hungry\`\n\`k!loli\`\n\`k!dab\`\n\`k!drunk\`\n\`k!triggered\`\n\`k!endme\`', true)
   const commonUser = new Discord.RichEmbed()
   commonUser.setTitle(`**アホイ\(Ahoi\) ${message.author.username}**`)
-  commonUser.setAuthor('BOT done by Wizardλ#4559', 'https://cdn.discordapp.com/avatars/267727230296129536/d97af37c3284d8c18a7c314c7c3f7174.jpg?size=1024')
+  commonUser.setAuthor('Click here to join the support server!', `${message.author.avatarURL}`)
   commonUser.setColor(0x00AE86)
-  commonUser.setDescription(`If you want to use Tamer\(Mod\) commands ${message.author} you need the role \`Dragon Tamer\` assigned to yourself on ${message.guild.name}`)
-  commonUser.setFooter('Join my support server if you have any more questions \(https://discord.gg/uBdXdE9\)')
-  commonUser.setThumbnail('https://cdn.discordapp.com/attachments/269129409888256000/300482904679645185/2c35594538206f7f598be792bf203b6b638e9c07_hq.jpg')
-  commonUser.addField('Commands',
-    '\`k!ping\` see how long the bot takes to answer commands\n\`k!invite\` gets the bot link invite\n\`k!about\` a bit about **Kanna Kobayashi** and how to set it up')
-  commonUser.addField('\u200b', '\u200b', true)
+  commonUser.setDescription(`If you want to use Tamer\(Mod\) commands ${message.author} you need the role \`Dragon Tamer\` assigned to yourself on ${message.guild.name}`);
+  commonUser.setFooter('BOT done by Wizardλ#4559', 'https://cdn.discordapp.com/avatars/267727230296129536/d97af37c3284d8c18a7c314c7c3f7174.jpg?size=1024');
+  commonUser.setThumbnail('https://cdn.discordapp.com/attachments/269129409888256000/300482904679645185/2c35594538206f7f598be792bf203b6b638e9c07_hq.jpg');
+  commonUser.setURL('https://discord.gg/uBdXdE9')
+  commonUser.addField('Commands', '\`k!ping\` see how long the bot takes to answer commands\n\`k!invite\` gets the bot link invite\n\`k!about\` a bit about **Kanna Kobayashi** and how to set it up\n\`k!feedback <message>\` send a feedback to the BOT owner himself!\n');
+  commonUser.addField('Memes', '\`k!pregnant\`\n\`k!lewd\`\n\`k!hungry\`\n\`k!loli\`\n\`k!dab\`\n\`k!drunk\`\n\`k!triggered\`\n\`k!endme\`', true);
 
 //////////EVERYONE CAN DO //////////
   if (message.content.startsWith(prefix + 'pregnant')) {
     message.channel.sendMessage(`Fear FBI ${message.author}\nhttps://cdn.discordapp.com/attachments/279299921071570958/298142045938122753/memo.png`);
-  };
-
-  if (message.content.startsWith(prefix + 'lewd')) {
+  } else if (message.content.startsWith(prefix + 'lewd')) {
     message.channel.sendMessage('https://streamable.com/npvut');
-  };
-
-  if (message.content.startsWith(prefix + 'hungry')) {
+  } else if (message.content.startsWith(prefix + 'hungry')) {
     message.channel.sendMessage('Hoomans taste good as insects?\n\nhttps://cdn.discordapp.com/attachments/269129409888256000/300481191151271946/giphy_9.gif')
     .then(() => {
       message.channel.awaitMessages(response => response.content.startsWith('yes'), {
@@ -115,21 +110,17 @@ client.on('message', message => {
       })
     })
     .catch(console.error)
-  };
-
-  if (message.content.startsWith(prefix + 'loli')) {
+  } else if (message.content.startsWith(prefix + 'triggered')) {
+    message.channel.sendMessage(`**YYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYY**\n\nhttps://cdn.discordapp.com/attachments/299398003486097412/301061190434553856/1491411414847.gif`)
+  } else if (message.content.startsWith(prefix + 'endme')) {
+    message.channel.sendMessage(`https://cdn.discordapp.com/attachments/299398003486097412/300740618009837571/1491078423557.jpg`)
+  } else if (message.content.startsWith(prefix + 'loli')) {
     message.channel.sendMessage(`**Hmm... A hooman**\nhttps://cdn.discordapp.com/attachments/275135980481150976/299263322157744128/aKVvBp1_460s.png`);
-  };
-
-  if (message.content.startsWith(prefix + 'dab')) {
+  } else if (message.content.startsWith(prefix + 'dab')) {
     message.channel.sendMessage(`**_dabs_**\nhttps://cdn.discordapp.com/attachments/275135980481150976/299673416712192000/hDrOJlT0GvA.jpg`);
-  };
-
-  if (message.content.startsWith(prefix + 'drunk')) {
+  } else if (message.content.startsWith(prefix + 'drunk')) {
     message.channel.sendMessage(`**_You have to be this cute to be drunk:_**\n\nhttps://cdn.discordapp.com/attachments/275135980481150976/299673369706496001/9wl5E10yNqE.jpg`);
-  };
-
-  if (message.content.startsWith(prefix + 'ping')) {
+  } else if (message.content.startsWith(prefix + 'ping')) {
     message.channel.sendMessage('Eating insects...').then(msg => {
       msg.edit(`I took \`${msg.createdTimestamp - message.createdTimestamp} ms\` to eat all of them! ${message.guild.emojis.find('name', 'hugme')}`);
       return;
@@ -138,7 +129,10 @@ client.on('message', message => {
     message.channel.sendMessage(`**こんにちは(Kon'nichiwa) ${message.author}**\n\nI am **カンナカムイ\(Kanna Kamui\)** i live with Kobayashi-san... So call me **Kanna Kobayashi**!\n\nWell, i am at **Discord** to spread the love for anime and quiz!\nFirst of all, you need to assign to yourself a role called \"Dragon Tamer\"\nAfter you can do \`k!qstart\` to test if everything is good!\n\nTo set up a different character you have to do \`k!sfname <first name>\` to set the character first name, \`k!slname <last name>\` to set up the character last name and \`k!sqphoto <link>\` to set up the character photo, then just do \`k!qstart\`!\n\n**Join my support server if you have any questions!**\nhttps://discord.gg/uBdXdE9`)
   } else if (message.content.startsWith(prefix + 'invite')) {
     message.channel.sendMessage(`Invite me to your server ${message.author}! ${message.guild.emojis.find('name', 'hugme')}\nhttps://discordapp.com/oauth2/authorize?permissions=1341643977&scope=bot&client_id=297459926505095180`)
-  };
+  } else if (message.content.startsWith(prefix + 'feedback')) {
+    client.users.get('267727230296129536').send(`**New feedback from the guild ${message.guild.name}**\n\n${message.content.split(prefix + 'feedback').slice(1)}\n\n**Info:**\nName: ${message.author.username}\nUser ID: ${message.author.id}\nGuild ID: ${message.guild.id}`)
+    message.channel.sendMessage(`A feedback has been sent to the overlord!`)
+  }
 
   if (message.author.id === '267727230296129536') {
     if (message.content.startsWith(prefix + 'sg')) {
